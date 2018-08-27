02058c95e2c0bb2140bc465055a3548f71a5e3e0
Deyan Dobromirov <dvd_video@abv.bg>
2018-08-27 12:12:38 +0300

    Added: Converted API expression Updated: piston generation API

--- lua/entities/gmod_wire_expression2/core/custom/wire_e2_piston_timing.lua
+++ lua/entities/gmod_wire_expression2/core/custom/wire_e2_piston_timing.lua
@@ -28,38 +28,39 @@ local function getData(oE, iD) local tP = oE[sK]
 end
 
 local function setData(oE, iD, oV)
-  if(iD) then oE[sK][iD] = oV else oE[sK] = oV end
+  if(iD) then oE[sK][iD] = oV else oE[sK] = oV end; return oE
 end
 
 local function setPiston(oE, iD, nH, bW)
-  if(not isEntity(oE)) then return end
+  if(not isEntity(oE)) then return nil end
   local tP = getData(oE); if(not tP) then
     setData(oE, nil, {}); tP = getData(oE) end
   local nL, iS = getAngNorm(nH + 180), 0
   if(nH > 0) then iS = 1 elseif(nH < 0) then iS = 2 else iS = 3 end
-  setData(oE, iD, {nH, nL, tF[iS], tobool(bW)})
+  return setData(oE, iD, {nH, nL, tF[iS], tobool(bW)})
 end
 
-local function getPiston(oE, iD, nB)
+local function getPiston(oE, iD, nB, iP)
   if(not isEntity(oE)) then return 0 end
   local tP = getData(oE, iD); if(not tP) then return 0 end
+  if(iP) then return (tP[iP] or 0) end
   if(tP[4]) then return getWave(nB, tP[1]) end
   return tP[3](nB, tP[1], tP[2])
 end
 
-e2function void entity:setPistonSign(number iD, number nT)
+e2function entity entity:setPistonSign(number iD, number nT)
   return setPiston(this, iD, nT, false)
 end
 
-e2function void entity:setPistonSign(string iD, number nT)
+e2function entity entity:setPistonSign(string iD, number nT)
   return setPiston(this, iD, nT, false)
 end
 
-e2function void entity:setPistonWave(number iD, number nT)
+e2function entity entity:setPistonWave(number iD, number nT)
   return setPiston(this, iD, nT, true)
 end
 
-e2function void entity:setPistonWave(string iD, number nT)
+e2function entity entity:setPistonWave(string iD, number nT)
   return setPiston(this, iD, nT, true)
 end
 
@@ -71,22 +72,46 @@ e2function number entity:getPiston(string iD, number nB)
   return getPiston(this, iD, nB)
 end
 
-e2function void entity:remPiston(number iD)
-  if(not isEntity(this)) then return end
-  local tP = getData(this); if(not tP) then return end
-  setData(this, iD, nil)
+e2function number entity:lowPiston(number iD)
+  return getPiston(this, iD, nil, 2)
 end
 
-e2function void entity:remPiston(string iD)
-  if(not isEntity(this)) then return end
-  local tP = getData(this); if(not tP) then return end
-  setData(this, iD, nil)
+e2function number entity:lowPiston(string iD)
+  return getPiston(this, iD, nil, 2)
 end
 
-e2function void entity:clrPiston()
-  if(not isEntity(this)) then return end
-  if(not getData(this)) then return end
-  setData(this, nil, nil)
+e2function number entity:higPiston(number iD)
+  return getPiston(this, iD, nil, 1)
+end
+
+e2function number entity:higPiston(string iD)
+  return getPiston(this, iD, nil, 1)
+end
+
+e2function number entity:wavPiston(number iD)
+  return (getPiston(this, iD, nil, 4) and 1 or 0)
+end
+
+e2function number entity:wavPiston(string iD)
+  return (getPiston(this, iD, nil, 4) and 1 or 0)
+end
+
+e2function entity entity:remPiston(number iD)
+  if(not isEntity(this)) then return nil end
+  local tP = getData(this); if(not tP) then return nil end
+  return setData(this, iD, nil)
+end
+
+e2function entity entity:remPiston(string iD)
+  if(not isEntity(this)) then return nil end
+  local tP = getData(this); if(not tP) then return nil end
+  return setData(this, iD, nil)
+end
+
+e2function entity entity:clrPiston()
+  if(not isEntity(this)) then return nil end
+  if(not getData(this)) then return nil end
+  return setData(this, nil, nil)
 end
 
 e2function number entity:cntPiston()
