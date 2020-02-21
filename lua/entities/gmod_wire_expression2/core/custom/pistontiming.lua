613a49bf76e7479f3aadcba65e15cda9dffe76b9
dvdvideo1234 <dvd_video@abv.bg>
2020-02-21 17:46:52 +0200

    Reduced: The number of locals in the mod file

--- lua/entities/gmod_wire_expression2/core/custom/pistontiming.lua
+++ lua/entities/gmod_wire_expression2/core/custom/pistontiming.lua
@@ -1,28 +1,20 @@
 --[[ **************************** CONFIGURATION **************************** ]]
 
-local tableConcat = table and table.concat
-local tableCopy   = table and table.Copy
-local mathSqrt    = math and math.sqrt
-local mathSin     = math and math.sin
-local mathAbs     = math and math.abs
-local bitBor      = bit and bit.bor
-local gnD2R       = (math.pi / 180)
-local gsName      = "pistontiming"
-local gsKey       = "wire_expression2_"..gsName
-local gtChipInfo  = {} -- Stores the global information for every E2
-local gtRoutines  = {} -- Stores global piston routines information
-local gvRoll, gvHigh = Vector(), Vector()
-local gvAxis, gwZero = Vector(), {0,0,0}
-
 E2Lib.RegisterExtension(gsName, true,
   "Allows E2 chips to attach pistons to the engine crankshaft props",
   "Configures prop engine pistons without messy boolean control conditions. Uses dedicated routines for each piston type."
 )
 
 -- Client and server have independent value
-local gnIndependentUsed = bitBor(FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_PRINTABLEONLY)
+local gnIndependentUsed = bit.bor(FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_PRINTABLEONLY)
 -- Server tells the client what value to use
-local gnServerControled = bitBor(FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_PRINTABLEONLY, FCVAR_REPLICATED)
+local gnServerControled = bit.bor(FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_PRINTABLEONLY, FCVAR_REPLICATED)
+
+local gnD2R       = (math.pi / 180)
+local gsName      = "pistontiming"
+local gsKey       = "wire_expression2_"..gsName
+local gtChipInfo  = {} -- Stores the global information for every E2
+local gtRoutines  = {} -- Stores global piston routines information
 local varEnStatus = CreateConVar(gsKey.."_enst",  0, gnIndependentUsed, "Enables status output messages")
 local varDefPrint = CreateConVar(gsKey.."_dprn", "TALK", gnServerControled, "FTrace default status output")
 local gsFormLogs  = "E2{%s}{%s}:piston: %s" -- Contains the logs format of the addon
@@ -53,6 +45,18 @@ local function logStatus(sMsg, oSelf, nPos, ...) -- logError
   end; return ...
 end
 
+function getExpressionSpot(oSelf)
+  local oRefr = oSelf.entity
+  local tSpot = gtChipInfo[oRefr]
+  if(not isHere(tSpot)) then  -- Check expression chip spot
+    gtChipInfo[oRefr] = {}    -- Allocate table when not available
+    tSpot = gtChipInfo[oRefr] -- Refer the allocated table to store into
+    tSpot.Axis = {0,0,0}      -- Rotation axis stored as a local vector relative to BASE
+    tSpot.Mark = {0,0,0}      -- Roll zero-mark stored as a local vector relative to SHAFT
+    tSpot.Base = nil          -- Entity for overloading and also the engine BASE entity
+  end; return tSpot           -- Return expression chip dedicated spot
+end
+
 local function isValid(oE)
   return (oE and oE:IsValid())
 end
@@ -77,12 +81,12 @@ local function getWireXYZ(nX, nY, nZ)
 end
 
 local function getWireCopy(tV)
-  return (tV and tableCopy(tV) or tableCopy(gwZero))
+  return (tV and table.Copy(tV) or getWireXYZ())
 end
 
 local function getWireAbs(tV) local nN = 0
-  for iD = 1, 3 do nN = nN + (tV[iD] or 0)^2 end
-  return mathSqrt(nN)
+  for iD = 1, 3 do nN = nN + (tonumber(tV[iD]) or 0)^2 end
+  return math.sqrt(nN)
 end
 
 local function setWireDiv(tV, vN)
@@ -96,12 +100,6 @@ local function setWireXYZ(tV, nX, nY, nZ)
   tV[3] = (tonumber(nZ) or 0); return tV
 end
 
-local function setVecWire(vV, tV)
-  vV.x = (tonumber(tV[1]) or 0)
-  vV.y = (tonumber(tV[2]) or 0)
-  vV.z = (tonumber(tV[3]) or 0); return vV
-end
-
 local function isWireZero(tV)
   local bX = ((tonumber(tV[1]) or 0) == 0)
   local bY = ((tonumber(tV[2]) or 0) == 0)
@@ -109,11 +107,12 @@ local function isWireZero(tV)
   return (bX and bY and bZ)
 end
 
+local gvRoll, gvHigh, gvAxis = Vector(), Vector(), Vector()
 local function getWireCross(tR, tH, tA)
-  local vR = setVecWire(gvRoll, tR); vR:Normalize()
-  local vH = setVecWire(gvHigh, tH) -- Normalized on creation
-  local vA = setVecWire(gvAxis, tA) -- Normalized on creation
-  return vH:Cross(vR):Dot(vA)
+  gvRoll:SetUnpacked(unpack(tR)); gvRoll:Normalize()
+  gvHigh:SetUnpacked(unpack(tH)) -- Normalized on creation
+  gvAxis:SetUnpacked(unpack(tA)) -- Normalized on creation
+  return gvHigh:Cross(gvRoll):Dot(gvAxis)
 end
 
 --[[ Converts the mark vector local to the SHAFT entity to
@@ -128,19 +127,7 @@ local function getMarkBase(tV, oE, oB)
   local vM = Vector(tV[1], tV[2], tV[3])
   vM:Rotate(oE:GetAngles()); vM:Add(oB:GetPos())
   vM:Set(oB:WorldToLocal(vM))
-  return getWireXYZ(vM.x, vM.y, vM.z)
-end
-
-function getSpot(oSelf)
-  local oRefr = oSelf.entity
-  local tSpot = gtChipInfo[oRefr]
-  if(not isHere(tSpot)) then  -- Check expression chip spot
-    gtChipInfo[oRefr] = {}    -- Allocate table when not available
-    tSpot = gtChipInfo[oRefr] -- Refer the allocated table to store into
-    tSpot.Axis = {0,0,0}      -- Rotation axis stored as a local vector relative to BASE
-    tSpot.Mark = {0,0,0}      -- Roll zero-mark stored as a local vector relative to SHAFT
-    tSpot.Base = nil          -- Entity for overloading and also the engine BASE entity
-  end; return tSpot           -- Return expression chip dedicated spot
+  return getWireXYZ(vM:Unpack())
 end
 
 local function getData(oE, iD) local tP = oE[gsKey]
@@ -154,11 +141,11 @@ end
 
 --[[ **************************** PISTON ROUTINES ****************************
 
-R   -> Roll value of the SHAFT entity
-H   -> Roll value for the piston highest point ( vector or number )
-L   -> Roll value for the piston lowest point ( vector or number )
-M   -> Piston initialization mode for the routine issued by the user
-A   -> Axis issued by the cross product timings in local coordinates
+ R  -> Roll value of the SHAFT entity
+ H  -> Roll value for the piston highest point ( vector or number )
+ L  -> Roll value for the piston lowest point ( vector or number )
+ M  -> Piston initialization mode for the routine issued by the user
+ A  -> Axis issued by the cross product timings in local coordinates
 [1] -> Contains the evaluation function definition for the given mode
 [2] -> Contains the internal data interpretation type for output calculation
 
@@ -167,14 +154,14 @@ A   -> Axis issued by the cross product timings in local coordinates
 -- Sign mode [nM=1] https://en.wikipedia.org/wiki/Square_wave
 gtRoutines[1] = {
 function(R, H, L, M, A) local nA = getAngNorm(R - H)
-  local nB, aA = ((nA >= 0) and 1 or -1), mathAbs(nA)
+  local nB, aA = ((nA >= 0) and 1 or -1), math.abs(nA)
   return ((aA == 0 or aA == 180) and 0 or nB)
 end, "number" }
 
 -- Wave mode [nM=2] https://en.wikipedia.org/wiki/Sine_wave
 gtRoutines[2] = {
 function(R, H, L, M, A)
-  return mathSin(gnD2R * getAngNorm(R - H))
+  return math.sin(gnD2R * getAngNorm(R - H))
 end, "number" }
 
 -- Cross product wave mode [nM=3] https://en.wikipedia.org/wiki/Sine_wave
@@ -192,7 +179,7 @@ end, "vector" }
 -- Direct ramp force mode [nM=5] https://en.wikipedia.org/wiki/Triangle_wave
 gtRoutines[5] = {
 function(R, H, L, M, A) local nN = getAngNorm(R - H)
-  local nA, nM = -getAngNorm(nN + 180), mathAbs(nN)
+  local nA, nM = -getAngNorm(nN + 180), math.abs(nN)
   return (((nM > 90) and nA or nN) / 90)
 end, "number" }
 
@@ -214,11 +201,11 @@ local function setPistonData(oS, oE, iD, oT, nM, oA)
     setData(oE, nil, {}); tP = getData(oE) end
   local nM = (tonumber(nM) or 0) -- Switch initialization mode
   if(nM == 1 or nM == 2 or nM == 5) then -- Sign [1], sine [2] ramp [5] (number)
-    vH, vL = oT, getAngNorm(oT + 180) -- Normalize the high and low angle
+    vH, vL, vA = oT, getAngNorm(oT + 180), nil -- Normalize the high and low angle
   elseif(nM == 3 or nM == 4) then -- Cross product [3], [4] (vector)
     if(isWireZero(oT)) then return logStatus("High vector zero", oS) end
     if(isWireZero(oA)) then return logStatus("Axis vector zero", oS) end
-    vH = setWireDiv({ oT[1], oT[2], oT[3]})
+    vH = setWireDiv({ oT[1], oT[2], oT[3]}) -- The top vector location
     vL = setWireDiv({-oT[1],-oT[2],-oT[3]})
     vA = setWireDiv({ oA[1], oA[2], oA[3]})
   else return logStatus("Mode ["..tostring(nM).."] not supported", oS) end
@@ -244,26 +231,26 @@ __e2setcost(1)
 e2function entity entity:setPistonBase(entity oB)
   if(not isValid(this)) then return nil end
   if(not isValid(oB)) then return nil end
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   tSpot.Base = oB; return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonBase()
   if(not isValid(this)) then return nil end
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   tSpot.Base = this; return this
 end
 
 __e2setcost(1)
 e2function entity entity:resPistonBase()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   tSpot.Base  = nil; return this
 end
 
 __e2setcost(1)
 e2function entity entity:getPistonBase()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   local oB = tSpot.Base -- Read base entity
   if(isValid(oB)) then return oB end
   return nil -- There is no valid base entity
@@ -273,49 +260,49 @@ end
 
 __e2setcost(5)
 e2function vector entity:getPistonAxis()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return tSpot.Axis
 end
 
 __e2setcost(1)
 e2function entity entity:resPistonAxis()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireXYZ(tSpot.Axis, 0, 0, 0); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(vector vA)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, vA[1], vA[2], vA[3])); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(vector2 vA)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, vA[1], vA[2], 0)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(array vA)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, vA[1], vA[2], vA[3])); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(number X, number Y, number Z)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, X, Y, Z)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(number X, number Y)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, X, Y, 0)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonAxis(number X)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Axis, X, 0, 0)); return this
 end
 
@@ -323,49 +310,49 @@ end
 
 __e2setcost(5)
 e2function vector entity:getPistonMark()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return tSpot.Mark
 end
 
 __e2setcost(1)
 e2function entity entity:resPistonMark()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireXYZ(tSpot.Mark, 0, 0, 0); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(vector vM)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, vM[1], vM[2], vM[3])); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(vector2 vM)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, vM[1], vM[2], 0)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(array vM)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, vM[1], vM[2], vM[3])); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(number X, number Y, number Z)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, X, Y, Z)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(number X, number Y)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, X, Y, 0)); return this
 end
 
 __e2setcost(1)
 e2function entity entity:setPistonMark(number X)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   setWireDiv(setWireXYZ(tSpot.Mark, X, 0, 0)); return this
 end
 
@@ -378,25 +365,25 @@ end
 
 __e2setcost(6)
 e2function vector entity:cnvPistonMark(vector vM)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return getMarkBase(vM, this, tSpot.Base)
 end
 
 __e2setcost(7)
 e2function vector entity:cnvPistonMark(number X, number Y, number Z)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return getMarkBase(getWireXYZ(X, Y, Z), this, tSpot.Base)
 end
 
 __e2setcost(6)
 e2function vector entity:cnvPistonMark(entity oB)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return getMarkBase(tSpot.Mark, this, oB)
 end
 
 __e2setcost(6)
 e2function vector entity:cnvPistonMark()
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return getMarkBase(tSpot.Mark, this, tSpot.Base)
 end
 
@@ -424,13 +411,13 @@ end
 
 __e2setcost(20)
 e2function entity entity:setPistonWaveX(number iD, vector vT)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return setPistonData(self, this, iD, vT, 3, tSpot.Axis)
 end
 
 __e2setcost(20)
 e2function entity entity:setPistonWaveX(string iD, vector vT)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return setPistonData(self, this, iD, vT, 3, tSpot.Axis)
 end
 
@@ -446,13 +433,13 @@ end
 
 __e2setcost(20)
 e2function entity entity:setPistonSignX(number iD, vector vT)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return setPistonData(self, this, iD, vT, 4, tSpot.Axis)
 end
 
 __e2setcost(20)
 e2function entity entity:setPistonSignX(string iD, vector vT)
-  local tSpot = getSpot(self)
+  local tSpot = getExpressionSpot(self)
   return setPistonData(self, this, iD, vT, 4, tSpot.Axis)
 end
 
